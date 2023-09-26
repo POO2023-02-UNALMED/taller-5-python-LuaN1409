d4db7fa4231bc65d975fba10fba74b6ccc1aee53
LuaN1409 <141962309+LuaN1409@users.noreply.github.com>
2023-09-26 11:04:27 -0500

    c

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -1,9 +1,9 @@
-from zooAnimales.Anfibio import Anfibio
-from zooAnimales.Ave import Ave
-from zooAnimales.Mamifero import Mamifero
-from zooAnimales.Pez import Pez
-from zooAnimales.Reptil import Reptil
-from gestion.Zona import Zona
+from zooAnimales.anfibio import Anfibio
+from zooAnimales.ave import Ave
+from zooAnimales.mamifero import Mamifero
+from zooAnimales.pez import Pez
+from zooAnimales.reptil import Reptil
+from gestion.zona import Zona
 
 class Animal:
     totalAnimales = 0
